22767a4fe2fc9dc16149b01f6a0fc11e904af328
Otavio <otavio.mlemos20@gmail.com>
2024-02-25 22:23:12 -0300

    adicionando anotacao @EntityScan, agora esta gerando as tabelas no banco de dados

--- src/main/java/com/aagterra/transportes/TransportesApplication.java
+++ src/main/java/com/aagterra/transportes/TransportesApplication.java
@@ -2,8 +2,10 @@ package com.aagterra.transportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"model.tms.despesas", "model.tms.faturamento", "model.tms.socios"})
 public class TransportesApplication {
 
 	public static void main(String[] args) {
