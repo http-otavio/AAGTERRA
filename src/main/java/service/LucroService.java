3e31af267c1df4752f1223fc238d330962a5e59b
Otavio <otavio.mlemos20@gmail.com>
2024-02-26 21:29:02 -0300

    alterando logica de disribuição de lucros

--- src/main/java/service/LucroService.java
+++ src/main/java/service/LucroService.java
@@ -2,19 +2,23 @@ package service;
 
 import exception.ResourceNotFoundException;
 import model.tms.socios.Lucro;
+import model.tms.socios.Socio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.LucroRepository;
+import repository.SocioRepository;
 
 import java.util.List;
 
 @Service
 public class LucroService {
     private final LucroRepository lucroRepository;
+    private final SocioRepository socioRepository;
 
     @Autowired
-    public LucroService(LucroRepository lucroRepository) {
+    public LucroService(LucroRepository lucroRepository, SocioRepository socioRepository) {
         this.lucroRepository = lucroRepository;
+        this.socioRepository = socioRepository;
     }
 
     public List<Lucro> getLucros() {
@@ -42,4 +46,22 @@ public class LucroService {
         lucroRepository.deleteById(id);
     }
 
+    public void distribuirLucro(double valorTotal) {
+        List<Socio> socios = socioRepository.findAll();
+
+        if (socios.size() != 2) {
+            throw new IllegalArgumentException("O número de sócios deve ser exatamente 2");
+        }
+
+        // Definindo os percentuais de lucro para cada sócio
+        socios.get(0).setPercentualDeLucro(50.0);
+        socios.get(1).setPercentualDeLucro(50.0);
+
+        // Distribuindo o lucro para cada sócio
+        for (Socio socio : socios) {
+            double valorDoLucro = valorTotal * (socio.getPercentualDeLucro() / 100.0);
+            socio.setValorRecebido(valorDoLucro);
+            socioRepository.save(socio);
+        }
+    }
 }
